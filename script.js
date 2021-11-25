642cb3b6e0707e6cb8da1a60f23d3c6892b55a9c
Maxime Mazzaro <maxime.mazzaro@gmail.com>
2021-11-25 11:08:04 +0100

    ..

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//!! Files in both codebases but very different !!
+
 const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minEl = document.getElementById("min");
@@ -36,4 +38,39 @@ function countDown () {
 }
 
 countDown();
-setInterval(countDown, 1000);
+setInterval(countDown, 2000);
+
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+function reverseString(str) {
+    return str;
+}
+reverseString("hello");
+
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+ 
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+ 
+reverseString("hello");
